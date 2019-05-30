62726eecc9f89b88e4b13a0109fa56f4e173b104
mykindofscum <alvinkchan99@gmail.com>
2019-05-30 14:11:35 -0700

    Add update, edit, delete roast function

--- controllers/roasts.js
+++ controllers/roasts.js
@@ -5,10 +5,9 @@ module.exports = {
     show,
     new: newRoast,
     create,
-    delRoast,
-    // edit,
-    // update
-
+    delete: delRoast,
+    edit,
+    update
 };
 
 function index(req, res, next) {
@@ -23,8 +22,7 @@ function index(req, res, next) {
 }
 
 function show(req, res) {
-    Roast.findById(req.params.id, function(err, roast) {
-        console.log('req.params.id');
+    Roast.findById(req.params.id, (err, roast) => {
         if (err) return res.redirect('/roasts/show');
         res.render('roasts/show', {
             title: 'Roast Detail', 
@@ -45,32 +43,37 @@ function create(req, res) {
     var roast = new Roast(req.body);
     roast.save((err) => {
         if (err) return res.redirect('/roasts/new');
-        console.log('roast.save')
         res.redirect(`/roasts`);
     });
 }
 
-function delRoast(req, res, next) {
-    Roast.findOne({'roasts._id': req.params.id}, (err, student) => {
-        roasts.id(req.params.id).remove();
-        roast.save((err) => {
-            res.redirect('/roasts');
-        });
+function delRoast(req, res) {
+    Roast.findByIdAndRemove(req.params.id, (err) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/roasts');    
+        }
+        res.redirect('/roasts');
+    });
+};
+
+function edit(req, res) {
+    Roast.findById(req.params.id, (err, roast) => {
+        res.render('roasts/edit', {roast});
     });
 }
 
-// function edit(req, res) {
-//     res.render('roasts/edit', {
-//         roast: Roast.getOne(req.params.id),
-//         idx: req.params.id
-//     });
-// }
-
-// function update(req, res) {
-//     req.body.done = req.body.done === 'on';
-//     Roast.update(req.params.id, req.body);
-//     res.redirext('/roasts');
-// }
+function update(req, res) {
+    req.body.nowSharing = req.body.nowSharing === 'on';
+    
+    Roast.findByIdAndUpdate(req.params.id, req.body, (err, roast) => {    
+        if (err) {
+            console.log(err);
+            res.redirect('/roasts');
+        }
+        res.redirect('/roasts');
+    });
+}
 
 
 
